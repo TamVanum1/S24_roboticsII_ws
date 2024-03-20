d68a1b0052557735b4029535b6566eb897c5f816
Guanlin Li <13811829077@163.com>
2024-03-20 14:26:28 -0400

    update control dead zone

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -159,10 +159,13 @@ class TrackingNode(Node):
         
         # TODO: Update the control velocity command
         cmd_vel = Twist()
-        cmd_vel.linear.x = current_object_pose[0]-0.7
+        cmd_vel.linear.x = current_object_pose[0]-0.3
         cmd_vel.linear.y = current_object_pose[1] * 0.6
         cmd_vel.angular.z = current_object_pose[2]
-        string = str(current_object_pose[0]-0.7) + " " + str(current_object_pose[1]) + " " + str(current_object_pose[2])
+
+        cmd_vel.linear.y = 0 if abs(cmd_vel.linear.y) <= 0.06 else cmd_vel.linear.y
+        cmd_vel.linear.z = 0 if abs(cmd_vel.linear.z) <= 0.1 else cmd_vel.linear.z
+        string = str(current_object_pose[0]-0.3) + " " + str(current_object_pose[1]) + " " + str(current_object_pose[2])
         self.get_logger().info(string)
         return cmd_vel
     
